1a2e924bb4618e40403fd9a6b19b5b8bf84f3c73
lhuangjs <1191578955@qq.com>
2019-04-27 23:52:10 +0800

    mdi can be used

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
 import 'vuetify/src/stylus/app.styl'
 
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import '@mdi/font/css/materialdesignicons.css'
+
 Vue.use(Vuetify, {
   iconfont: 'mdi'
 })
